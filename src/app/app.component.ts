cc063859d6d8013d0d5e3f7dda940fe4605344aa
stewazy <stewazy@gmail.com>
2017-03-15 21:35:23 +0100

    finished Reddit test app - tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor () {
+    this.articles = [
+      new Article('Angular 2 Stevara Zbori','http://angular.io',3),
+      new Article('Fullstack najdobar sajt','http://fullstack.io',2),
+      new Article('Angular Home Page uci odavde','http://angular.io',1),
+    ];
+
+  }
+
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article (title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article [] {
+    return this.articles.sort((a: Article, b: Article)=>b.votes-a.votes);
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Article} from './article.model';
 
 @Component({
@@ -10,14 +10,7 @@ import {Article} from './article.model';
   }
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
-
-  constructor() {
-    this.article = new Article(
-      'Angular 2 Stevara Zbori',
-      'http://angular.io',
-      10);
-  }
+  @Input() article: Article;
 
   voteUp(): boolean {
     this.article.voteUp();
